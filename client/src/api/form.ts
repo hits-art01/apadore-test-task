14bb3d0205716754183d7d68561262deb7593ccd
Arthur <hitsuga2000@gmail.com>
2026-02-24 14:40:02 +0100

    fix

--- client/src/api/form.ts
+++ client/src/api/form.ts
@@ -1,13 +1,16 @@
 import type { FormData } from "../utils/validateForm";
 
 export const sendContactForm = async (formData: FormData) => {
-  const response = await fetch("https://apadore-test-task.onrender.com", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    "https://apadore-test-task.onrender.com/api/contact",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     },
-    body: JSON.stringify(formData),
-  });
+  );
 
   const data = await response.json();
 
